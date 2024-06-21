6fbf26bf5aaff2f4d765969dbc6d8ac6638f6e15
sajidahmad0000 <sajid01ahmad@gmail.com>
2024-06-21 23:56:15 +0530

    added globalErrorHandlers

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+
 const app = express();
 
 // Routes
 // http methods :  get ,post put, patch, delete
 app.get("/", (req, res, next) => {
+  //   const error = createHttpError(400, "something went WRong");
+  //   throw error;
   res.json({ message: "Welcom to E_LIB_API" });
 });
+
+// global error handlers should be after all routes you define
+app.use(globalErrorHandler); // express will call this we dont have to
 
 export default app;
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500; //app.use is for middlewares
+  return res.status(statusCode).json({
+    message: err.message,
+    errorStack: config.env === "development" ? err.stack : "",
+    // this errorStack has full details of error occurence and other things
+    // this should not be send this in production bcz it can expose vital info
+  });
+};
+
+export default globalErrorHandler;
